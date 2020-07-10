9ad6cf81cb59dab75b51c97ac265b69a1f90ba63
wondervictor <victorchanchina@gmail.com>
2020-07-10 21:03:00 +0800

    add rpn

--- configs/lvis/ga_rpn_r50_fpn_2x_lvis.py
+++ configs/lvis/ga_rpn_r50_fpn_2x_lvis.py
@@ -1,10 +1,5 @@
-_base_ = [
-    '../_base_/models/mask_rcnn_r50_fpn.py',
-    '../_base_/datasets/coco_instance.py',
-    '../_base_/default_runtime.py'
-]
+_base_ = './rpn_r50_fpn_2x_lvis.py'
 model = dict(
-    pretrained='pretrained_model/resnet50-19c8e357.pth',
     rpn_head=dict(
         _delete_=True,
         type='GARPNHead',
@@ -39,48 +34,8 @@ model = dict(
         loss_shape=dict(type='BoundedIoULoss', beta=0.2, loss_weight=1.0),
         loss_cls=dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
-    roi_head=dict(
-        bbox_head=dict(num_classes=1230, bbox_coder=dict(target_stds=[0.05, 0.05, 0.1, 0.1])),
-        mask_head=dict(num_classes=1230)))
-test_cfg = dict(
-    rcnn=dict(
-        score_thr=0.0001,
-        # LVIS allows up to 300
-        max_per_img=300))
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
-]
-dataset_type = 'LVISDataset'
-data_root = 'data/coco/'
-data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=2,
-    train=dict(
-        type=dataset_type,
-        ann_file=data_root + 'annotations/lvis_v0.5_train.json',
-        img_prefix=data_root + 'train2017/',
-        pipeline=train_pipeline),
-    val=dict(
-        type=dataset_type,
-        ann_file=data_root + 'annotations/lvis_v0.5_val.json',
-        img_prefix=data_root + 'val2017/'),
-    test=dict(
-        type=dataset_type,
-        ann_file=data_root + 'annotations/lvis_v0.5_val.json',
-        img_prefix=data_root + 'val2017/'))
-evaluation = dict(metric=['bbox', 'segm'])
-
-# train_config
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)))
+# model training and testing settings
 train_cfg = dict(
     rpn=dict(
         ga_assigner=dict(
@@ -97,14 +52,10 @@ train_cfg = dict(
             add_gt_as_proposals=False),
         allowed_border=-1,
         center_ratio=0.2,
-        ignore_ratio=0.5),
-    rpn_proposal=dict(max_num=300),
-    rcnn=dict(
-        assigner=dict(pos_iou_thr=0.6, neg_iou_thr=0.6, min_pos_iou=0.6),
-        sampler=dict(type='RandomSampler', num=256)))
-# schedule
+        ignore_ratio=0.5))
+
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
     policy='step',
@@ -113,5 +64,34 @@ lr_config = dict(
     warmup_ratio=0.001,
     step=[16, 22])
 total_epochs = 24
-
-# test_cfg = dict(rpn=dict(max_num=300), rcnn=dict(score_thr=1e-3))
+# data
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_label=False),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes']),
+]
+dataset_type = 'LVISDataset'
+data_root = 'data/coco/'
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/lvis_v0.5_train.json',
+        img_prefix=data_root + 'train2017/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/lvis_v0.5_val.json',
+        img_prefix=data_root + 'val2017/'),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/lvis_v0.5_val.json',
+        img_prefix=data_root + 'val2017/'))
---
+++ configs/lvis/rpn_r50_fpn_2x_lvis.py
@@ -0,0 +1,47 @@
+_base_ = [
+    '../_base_/models/rpn_r50_fpn.py', '../_base_/datasets/coco_detection.py',
+    '../_base_/default_runtime.py'
+]
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_label=False),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes']),
+]
+dataset_type = 'LVISDataset'
+data_root = 'data/coco/'
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/lvis_v0.5_train.json',
+        img_prefix=data_root + 'train2017/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/lvis_v0.5_val.json',
+        img_prefix=data_root + 'val2017/'),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/lvis_v0.5_val.json',
+        img_prefix=data_root + 'val2017/'))
+
+evaluation = dict(interval=1, metric='proposal_fast')
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[16, 22])
+total_epochs = 24
